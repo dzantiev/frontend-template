e35aa142a8e2896419e89ad89ed235e9cf830c42
CarolineUniverse1443 <popovakaro03@gmail.com>
2023-05-15 17:52:05 +0300

    add imask js to g-masked-input

--- ui/src/blocks/g-masked-input/g-masked-input.js
+++ ui/src/blocks/g-masked-input/g-masked-input.js
@@ -1,10 +1,23 @@
-import './vendors/jquery.maskedinput.min.js';
+// import './vendors/jquery.maskedinput.min.js';
+import IMask from 'imask';
 
 "use strict";
 o2.innputMask =
 {
 	init()
 	{
-		$('._phone-mask').mask('+7(999)999-99-99',{autoclear: false});
+		// $('._phone-mask').mask('+7(999)999-99-99',{autoclear: false});
+		let maskedInputs = document.querySelectorAll('._phone-mask');
+		let maskOptions = {
+			mask: '+{7}(000)000-00-00'
+		};
+
+		let masks = [];
+
+		for(let i = 0; i < maskedInputs.length; i++)
+		{
+			masks[i] = IMask(maskedInputs[i], maskOptions);
+			console.log(masks[i]);
+		}
 	}
 };
