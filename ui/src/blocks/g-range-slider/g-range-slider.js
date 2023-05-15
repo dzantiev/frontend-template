5df53236e64ebac31fda58a049da85b21c2b05ff
CarolineUniverse1443 <popovakaro03@gmail.com>
2023-05-15 10:06:21 +0300

    fix g-range-slider

--- ui/src/blocks/g-range-slider/g-range-slider.js
+++ ui/src/blocks/g-range-slider/g-range-slider.js
@@ -4,30 +4,6 @@ o2.gRangeSlider =
 {
 	init()
 	{
-		// $('._range-slider').each((index, element) =>
-		// {
-		// 	const dataParams = $(element).data();
-		// 	let start = $(element).data('start');
-		// 	let connect = [true,false];
-		// 	if(typeof start == 'string')
-		// 	{
-		// 		start = start.split(',');
-		// 		connect = true;
-		// 	}
-
-		// 	const slider = noUiSlider.create(element,{
-		// 		start    : start,
-		// 		connect  : connect,
-		// 		tooltips : true,
-		// 		format   : this.format,
-		// 		step     : 1,
-		// 		range    : {'min': dataParams.min, 'max': dataParams.max }
-		// 	});
-
-		// 	slider.on('set', this.set);
-		// 	slider.on('update', this.update);
-		// });
-
 		const sliders = document.querySelectorAll('._range-slider');
 
 		sliders.forEach((element) =>
@@ -59,7 +35,6 @@ o2.gRangeSlider =
 	{
 		if (handle == 0)
 		{
-			// $(this.target).siblings('._min').trigger('change');
 			const minSibling = this.target.nextElementSibling;
 			if (minSibling && minSibling.classList.contains('_min'))
 			{
@@ -69,7 +44,6 @@ o2.gRangeSlider =
 		}
 		else
 		{
-			// $(this.target).siblings('._max').trigger('change');
 			const maxSibling = this.target.nextElementSibling;
 			if (maxSibling && maxSibling.classList.contains('._max'))
 			{
@@ -79,26 +53,19 @@ o2.gRangeSlider =
 	},
 	update(values)
 	{
-		// let $slider = $(this.target).parents('._range-slider-wrap');
 		let slider = this.target.closest('._range-slider-wrap');
 
 		let firstValue = o2.gRangeSlider.format.from(values[0].toString());
-		// $slider.find('._min').val(firstValue);
-		// $slider.find('._label-to').html(values[0]);
 		slider.querySelector('._min').value = firstValue;
 		slider.querySelector('._label-to').innerHTML = values[0];
 
 
 		let from = o2.gRangeSlider.format.to(this.options.range.min);
-		// $slider.find('._label-from').html(from);
 		slider.querySelector('._label-from').innerHTML = from;
 
 		if(values.length > 1)
 		{
 			let secondVal = o2.gRangeSlider.format.from(values[1].toString());
-			// $slider.find('._max').val(secondVal);
-			// $slider.find('._label-from').html(values[0]);
-			// $slider.find('._label-to').html(values[1]);
 			slider.querySelector('._max').value = secondVal;
 			slider.querySelector('._label-from').innerHTML = values[0];
 			slider.querySelector('._label-to').innerHTML = values[1];
