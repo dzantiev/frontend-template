e3418fc7074f2ceaf455ecb216419469edd040f1
CarolineUniverse1443 <popovakaro03@gmail.com>
2023-05-17 15:01:52 +0300

    delete vendors

--- ui/src/blocks/g-slider/g-slider.js
+++ ui/src/blocks/g-slider/g-slider.js
@@ -3,14 +3,6 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 "use strict";
 o2.initSlider = function ()
 {
-	// $('.g-slider').slick({
-	// 	infinite: true,
-	// 	slidesToShow: 5,
-	// 	slidesToScroll: 3,
-	// 	prevArrow: '<div class="prev-arrow"></div>',
-	// 	nextArrow: '<div class="next-arrow"></div>'
-	// });
-
 	const slidesCount = document.querySelectorAll('.swiper .swiper-slide').length;
 
 	const mySwiper = new Swiper('.swiper', {
